94b8857558b8ab947c4ebbfe414a8117e2d9ec56
Mohamad Bwidani <mbwedany@gmail.com>
2019-09-18 15:23:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,19 @@ class BoardMember{
     this.homeState = homeState;
     this.training = training;
   }
+  veto(){
+    return "No, I must disagree";
+  }
+  approve(){
+    return "You can do that!";
+  }
+  doCharity(){
+    return "I like to help people.";
+  }
+  releasePressStatement(){
+    return "You will see great things from Scuber.";
+  }
+  sayHi(){
+    return
+  }
 }
